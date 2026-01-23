e9a2890a84666176cc6722cb5c4e4eaffceef6fd
opurbo2512 <opurbo2512@gmail.com>
2026-01-23 23:16:12 +0600

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ with st.container():
     contact_form = """
     <form action="https://formsubmit.co/opurbo2512@gmail.com" method="POST">
         <input type="hidden" name="_captcha" value="false">
-        <input type="text" name="name" placeholder="Your nmae" required>
+        <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
         <textarea name="message" placeholder="Your message here" required></textarea>
         <button type="submit">Send</button>
